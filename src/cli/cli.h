5ba226d383ad4d9470056e4ae5d09aa53c67c24f
Mattifrog <ranamattia2006@gmail.com>
2026-05-22 20:58:45 +0200

    inizializzazione cli

--- src/cli/cli.h
+++ src/cli/cli.h
@@ -1,4 +1,11 @@
 #ifndef CLI_H
 #define CLI_H
+#include"ware.h"
+
+void show_startup_screen(void);
+void run_main_menu(AssistanceRequest *list);
+void show_error_message(const char *message);
+void show_success_message(const char *message);
+void show_request_message(const char *message);
 
 #endif //CLI_H
